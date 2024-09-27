dd48d0f6ea817c64ef459552ec74c677c523af22
Samuel Javorka <samkoj@hotmail.co.uk>
2024-09-27 23:57:48 +0100

    Shuffle question answers, check and render on quizIsComplete

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -5,10 +5,24 @@ export default function Quiz() {
 	const [userAnswers, setUserAnswers] = useState([]);
 	const activeQuestionIndex = userAnswers.length;
 	const question = QUESTIONS[activeQuestionIndex];
+	const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
 	function handleSelectAnswer(answer) {
 		setUserAnswers((prev) => [...prev, answer]);
 	}
+
+	if (quizIsComplete) {
+		return (
+			<div id="summary">
+				<h2>Quiz Completed!</h2>
+			</div>
+		);
+	}
+
+	const shuffledAnswers = [...question.answers];
+	// in the sort callback, if you return negative, elements swap, positive, elements stay same
+	// shuffle the answers by swaping the elements, each element has 50% chance to swap
+	shuffledAnswers.sort((a, b) => Math.random() - 0.5);
 
 	return (
 		<div id="question-container">
